7ddd77c29da8068afb7e3ab503a5d60a53dd9da8
JulienDFS29C <154337402+JulienDFS29C@users.noreply.github.com>
2024-03-29 16:30:07 +0100

    Create indexTemplate.php

---
+++ indexTemplate.php
@@ -0,0 +1,21 @@
+<?php
+$server_name = "sthg";
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to' .$server_name.'!</title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>Welcome to' .$server_name.'!</h1>
+
+</body>
+</html>
+'
+/?>
