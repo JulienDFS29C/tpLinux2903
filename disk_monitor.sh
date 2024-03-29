d88372220d4e6b0819c656b2f57ce2d714018a66
JulienDFS29C <154337402+JulienDFS29C@users.noreply.github.com>
2024-03-29 15:22:43 +0100

    Update disk_monitor.sh

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -2,4 +2,12 @@
 
 percentage=$(df / --output=pcent | sed -n '2p' | tr -d ' %');
 
-echo "Disk space almost full : $percentage% used";
+message=$(echo "Disk space almost full : $percentage% used");
+
+echo $percentage;
+
+if [ "$percentage" -gt 10 ]; then
+
+	slackUrl=$(echo "https://hooks.slack.com/services/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")
+	curl -X POST -H "Content-Type: application/json" --data '{"text":"Disk space almost full'$percentage'% used"}' --location $slackUrl
+fi
